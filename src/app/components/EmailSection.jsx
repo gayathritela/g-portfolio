ceb7ae202a95dfb2f804549f9df29730475e110b
gayathritela <gayathritela99@gmail.com>
2024-07-13 11:56:51 -0400

    Fixed unescaped HTML entities in JSX and updated components

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -22,9 +22,9 @@ const EmailSection = () => {
   return (
     <section id="contact" className="grid md:grid-cols-2 gap-4 py-12 mt-32 mb-24 relative bg-white bg-opacity-60 text-[#42325b] rounded-lg shadow-lg px-5 md:px-12">
       <div className="p-5 md:p-12">
-        <h5 className="text-3xl font-bold text-[#42325b] my-2">Let's Connect</h5>
+        <h5 className="text-3xl font-bold text-[#42325b] my-2">Let&apos;s Connect</h5>
         <p className="mb-4 max-w-md text-[#42325b]">
-          I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!
+          I&apos;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
         </p>
         <div className="flex flex-row gap-2">
           <a href="https://github.com/gayathritela" className="text-3xl text-[#42325b] hover:text-black cursor-pointer">
@@ -48,7 +48,7 @@ const EmailSection = () => {
           </div>
           <div>
             <label htmlFor="message" className="block mb-2 text-sm font-medium text-[#42325b]">Message</label>
-            <textarea name="message" id="message" required className="bg-gray-100 border border-gray-300 text-gray-900 rounded-lg block w-full p-3" placeholder="Let's talk about..." />
+            <textarea name="message" id="message" required className="bg-gray-100 border border-gray-300 text-gray-900 rounded-lg block w-full p-3" placeholder="Let&apos;s talk about..." />
           </div>
           <button type="submit" className="bg-[#7c6a99] hover:bg-[#563e7b] text-white font-bold py-3 px-5 rounded-lg w-full transition-colors duration-300">
             Send Message
